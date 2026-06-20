a11fbc8ad4431610a6cd4e3b474afd315df6d861
APPekala <s34784@pjwstk.edu.pl>
2026-06-20 21:57:40 +0200

    Update config.php

--- Notatnik/config/config.php
+++ Notatnik/config/config.php
@@ -7,18 +7,15 @@ define('DB_USER', '');
 define('DB_PASS', '');
 
 define('UPLOAD_DIR', __DIR__ . '/../public/uploads/');
-define('BASE_URL', '/notatnik/public/'); // dostosuj do ścieżki
+define('BASE_URL', '/notatnik/public/');
 
-// Ustawienia walidacji
 define('TITLE_MIN', 3);
 define('TITLE_MAX', 100);
 define('TAG_MAX', 50);
 define('PREVIEW_LENGTH', 60);
 
-// Priorytety dozwolone
 define('PRIORITIES', ['niski', 'średni', 'wysoki']);
 
-// Inicjalizacja sesji
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
